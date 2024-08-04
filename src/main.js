c71851d5c846c0d5c6b19cb026c0bf561acb6b3e
ArthurMykhailiuk <artur.myhajlyuk@volo.global>
2024-08-04 12:56:16 +0300

    Fix 1.0

--- src/main.js
+++ src/main.js
@@ -15,43 +15,49 @@ const galleryEl = document.querySelector('.js-gallery');
 const onSearchFormSubmit = event => {
   event.preventDefault();
 
-  const searchedValue = event.target.elements.user_query.value;
+  const searchedValue = event.target.elements.user_query.value.trim();
 
   event.target.elements.user_query.value = '';
 
   addProgressBar();
 
-  fetchPhotos(searchedValue)
-    .then(({ hits }) => {
-      if (hits.length === 0) {
-        iziToast.show({
-          title:
-            'Sorry, there are no images matching your search query. Please try again!',
-          position: 'topRight',
-          color: 'red',
+  if (searchedValue.length !== 0) {
+    fetchPhotos(searchedValue)
+      .then(({ hits }) => {
+        if (hits.length === 0) {
+          galleryEl.innerHTML = '';
+
+          removeProgressBar();
+
+          iziToast.show({
+            title:
+              'Sorry, there are no images matching your search query. Please try again!',
+            position: 'topRight',
+            color: 'red',
+          });
+
+          return;
+        }
+
+        const galleryCardsTemplate = hits
+          .map(imgInfo => createGalleryCardTemplate(imgInfo))
+          .join('');
+
+        galleryEl.innerHTML = galleryCardsTemplate;
+
+        removeProgressBar();
+
+        const lightbox = new SimpleLightbox('.gallery-card a', {
+          captionsData: 'alt',
+          captionDelay: 250,
         });
 
-        return;
-      }
-
-      const galleryCardsTemplate = hits
-        .map(imgInfo => createGalleryCardTemplate(imgInfo))
-        .join('');
-
-      galleryEl.innerHTML = galleryCardsTemplate;
-
-      const lightbox = new SimpleLightbox('.gallery-card a', {
-        captionsData: 'alt',
-        captionDelay: 250,
+        lightbox.refresh();
+      })
+      .catch(err => {
+        console.log(err);
       });
-
-      lightbox.refresh();
-    })
-    .catch(err => {
-      console.log(err);
-    });
-
-  removeProgressBar();
+  }
 };
 
 searchFormEl.addEventListener('submit', onSearchFormSubmit);
